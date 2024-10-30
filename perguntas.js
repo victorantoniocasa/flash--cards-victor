0ab417ade85d74da2e95f066cd73c1689f78473e
Victor antonio <149177594+victorantoniocasa@users.noreply.github.com>
2024-10-30 10:34:34 -0300

    30

--- perguntas.js
+++ perguntas.js
@@ -1,22 +1,22 @@
-criacartao(
+criaCartao(
 'População',
 'Qual e o tamanho da população de curitiba?',
 '1.829.225 de pessoas',
 )
 
-criacartao(
+criaCartao(
 'Carros',
 'Quantos cv tem um hb20 1.0?',
 'Potencia maxima de 80 cv',
 )
 
-criacartao(
+criaCartao(
 'Pescaria',
 'Qual e os melhores meses do ano para pescar?',
 'De outubro até o mes de março',
 )
 
-criacartao(
+criaCartao(
 'Motos',
 'Qual e a nova moto lançada na honda 2024?',
 'Honda tornado 300L', 
